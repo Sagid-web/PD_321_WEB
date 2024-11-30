5fb880c15d970d7c5c7fa04ed2cedf6282111082
Александр <@DESKTOP-7T7KAJU>
2024-11-30 17:52:24 +0500

    Gallery DONE

--- ASP/MvcMovie/Models/Movie.cs
+++ ASP/MvcMovie/Models/Movie.cs
@@ -1,14 +1,20 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MvcMovie.Models;
 
 public class Movie
 {
 	public int Id { get; set; }
+	[StringLength(60,MinimumLength = 3)]
+	[Required]
 	public required string Title { get; set; }
+	[Display(Name = "Realease date")]
 	[DataType(DataType.Date)]
-	public DateTime? ReleaseDate { get; set; }	//Nullable fields
+	public DateTime? ReleaseDate { get; set; } //Nullable fields
+	[RegularExpression(@"[A-Z+[a-zA-Z\s]*$")]
 	public string? Genre {  get; set; }
 	[Required]
 	public decimal Price { get; set; }
+	public string? Rating { get; set; }	
 }
